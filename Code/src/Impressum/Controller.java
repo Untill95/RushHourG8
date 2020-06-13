7b418538ec42492d2183693e8bcfa80967e641a6
Untill95 <63193155+Untill95@users.noreply.github.com>
2020-06-13 15:02:26 +0200

    Add files via upload

---
+++ Code/src/Impressum/Controller.java
@@ -0,0 +1,10 @@
+package Impressum;
+
+import java.awt.event.ActionEvent;
+
+public class Controller {
+    public static void pressButton ( ActionEvent event ){
+        System.out.println("HelloWorld");
+    }
+
+}
---
+++ Code/src/Main.java
@@ -0,0 +1,12 @@
+import Impressum.Controller;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class Main {
+    public static void main ( String[] args ) {
+
+        ActionEvent event;
+        Controller.pressButton(event);
+    }
+}
